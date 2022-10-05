0d178b67bb1dbbb829151c7b4706ec27cb2c9b69
Núbia <112019351+nu-Bia@users.noreply.github.com>
2022-10-05 15:11:13 -0300

    Add files via upload

---
+++ multiplicação.js
@@ -0,0 +1,6 @@
+function multiplique(){
+    var numero1=prompt(("digite um numero:"));
+    var numero2=(prompt("digite um numero:"));
+    var multiplique=numero1*numero2;
+    alert(" O valor da multiplicação é :"+multiplique);
+    }
